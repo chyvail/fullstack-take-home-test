74a1d7631a3a27e9a59b62ee2b0c835dd391fd9a
joshua chivile <joshuachivile@gmail.com>
2024-06-11 17:23:03 +0300

    Add search functionality with useEffect

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTheme } from "@emotion/react";
 import { Box, IconButton, InputBase } from "@mui/material";
 import { IoMdSearch } from "react-icons/io";
+import SearchList from "./SearchList";
 
 // Search Bar Component
 
-export default function Search() {
+export default function Search({ data }) {
   const theme = useTheme();
+  const [input, setInput] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  console.log(data);
+
+  // UseEffect Hook to handle Search Requests
+
+  useEffect(() => {
+    if (input) {
+      const handleSearch = () => {
+        const filtered = data.books.filter((book) =>
+          book.title.toLowerCase().includes(input.toLowerCase())
+        );
+        setFilteredBooks(filtered);
+      };
+      handleSearch();
+    }
+  }, [input, data.books]);
+
   return (
-    <Box display="flex" borderRadius="50px" width="100%" border={2}>
-      <IconButton
-        type="button"
-        sx={{ margin: "8px", background: theme.palette.primary.dark }}
-      >
-        <IoMdSearch color={theme.palette.primary.contrastText} />
-      </IconButton>
-      <InputBase
-        placeholder="Search Everything in our catalog"
-        sx={{ flex: 1, padding: "0 10px" }}
-      />
+    <Box position="relative" sx={{ width: { xs: "100%", sm: "50%" } }}>
+      <Box display="flex" borderRadius="50px" border={2} width="100%">
+        <IconButton
+          type="button"
+          sx={{ margin: "8px", background: theme.palette.primary.dark }}
+        >
+          <IoMdSearch color={theme.palette.primary.contrastText} />
+        </IconButton>
+        <InputBase
+          placeholder="Search Everything in our catalog"
+          sx={{ flex: 1, padding: "0 10px" }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </Box>
+      <Box position="absolute" width="100%" zIndex={2}>
+        {input && <SearchList data={filteredBooks} />}
+      </Box>
     </Box>
   );
 }
---
+++ frontend/src/components/SearchList.jsx
@@ -0,0 +1,64 @@
+import { useTheme } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+// Search List
+
+export default function SearchList({ data }) {
+  const theme = useTheme();
+  return (
+    <Box
+      mt={2}
+      borderRadius={2}
+      width="100%"
+      sx={{
+        maxHeight: "250px",
+        overflowY: "scroll",
+      }}
+      className="custom-shadow"
+    >
+      {data.map((book, index) => (
+        <Box
+          display="flex"
+          p={1}
+          key={index}
+          sx={{ background: `${theme.palette.primary.contrastText}` }}
+        >
+          <Box
+            component="img"
+            src={book.coverPhotoURL}
+            alt={book.title}
+            sx={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+              marginBottom: "10px",
+              display: {
+                xs: "none",
+                sm: "block",
+              },
+            }}
+          />
+          <Box ml={1}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "600",
+              }}
+              color="primary"
+            >
+              {book.title}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ textTransform: "uppercase" }}
+              color="primary"
+            >
+              {book.author}
+            </Typography>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+}
